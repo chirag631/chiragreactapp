9b1efd4bbbc91ba3e79c749c2d6daeaa288d4161
chirag631 <05042000cdk@gmail.com>
2020-09-03 23:47:34 +0530

    updt

---
+++ precache-manifest.3b2680fe760aaacaf3908b1bf4fe70bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34db49465c2dcc39785feb5a2a593fd8",
+    "url": "/chiragreactapp/index.html"
+  },
+  {
+    "revision": "dd9b77ff1004f8b0f715",
+    "url": "/chiragreactapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6d9befc9ace51eb9d447",
+    "url": "/chiragreactapp/static/css/main.5b63eaa1.chunk.css"
+  },
+  {
+    "revision": "dd9b77ff1004f8b0f715",
+    "url": "/chiragreactapp/static/js/2.cc98d32c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chiragreactapp/static/js/2.cc98d32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9befc9ace51eb9d447",
+    "url": "/chiragreactapp/static/js/main.73a3c210.chunk.js"
+  },
+  {
+    "revision": "3de1952b2d6273ee9358",
+    "url": "/chiragreactapp/static/js/runtime-main.beff7f4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chiragreactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e974a5f707e8ef934966e7067535fff.js"
+  "/chiragreactapp/precache-manifest.3b2680fe760aaacaf3908b1bf4fe70bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chiragreactapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
